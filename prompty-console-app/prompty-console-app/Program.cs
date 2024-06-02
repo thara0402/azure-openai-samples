fed6506151a9796e9c9febff19606cae18584a34
thara0402 <thara0402@gmail.com>
2024-06-02 17:02:02 +0900

    add prompty

---
+++ prompty-console-app/prompty-console-app/Program.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable SKEXP0040
+using Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using prompty_console_app;
+using static System.Net.Mime.MediaTypeNames;
+
+var settings = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build()
+    .GetSection(nameof(AzureOpenAISettings)).Get<AzureOpenAISettings>() ?? throw new NullReferenceException();
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(settings.DeploymentName, settings.Endpoint, settings.ApiKey);
+var kernel = builder.Build();
+
+var prompty = kernel.CreateFunctionFromPromptyFile("assistant.prompty");
+
+while (true)
+{
+    Console.Write("You: ");
+    var text = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(text))
+    {
+        break;
+    }
+
+    Console.WriteLine("Assistant:");
+    await foreach (var result in kernel.InvokeStreamingAsync<string>(prompty, new() { ["question"] = text }))
+    {
+        Console.Write(result);
+    }
+    Console.WriteLine("");
+}
